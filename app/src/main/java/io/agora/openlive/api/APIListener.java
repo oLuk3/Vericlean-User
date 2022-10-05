128f496b8410ba50d3056011f0147d1573b92dd3
oLuk3 <nathanocamps@gmail.com>
2022-10-05 23:30:39 +0800

    fix error

--- app/src/main/java/io/agora/openlive/api/APIListener.java
+++ app/src/main/java/io/agora/openlive/api/APIListener.java
@@ -1,7 +1,12 @@
 package io.agora.openlive.api;
 
+import java.util.List;
+
+import io.agora.openlive.Schedule;
 import io.agora.openlive.User;
 
 public interface APIListener {
     void onLogin(User user);
+
+   // void onScheduleLoaded(List<Schedule> schedules);
 }
